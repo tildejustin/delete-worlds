639933f84182121ce03703f183ef1cb97969e663
tildejustin <spacepepper3.14@gmail.com>
2024-05-29 21:03:00 -0400

    1.19-1.20.2 port

--- src/main/java/dev/tildejustin/delete_worlds/mixin/SelectWorldScreenMixin.java
+++ src/main/java/dev/tildejustin/delete_worlds/mixin/SelectWorldScreenMixin.java
@@ -8,24 +8,8 @@ import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 
-import java.lang.reflect.*;
-
-
 @Mixin(SelectWorldScreen.class)
 public abstract class SelectWorldScreenMixin extends Screen {
-    @Unique
-    private static final Method method_20170;
-
-    static {
-        try {
-            // lambda in WorldListWidget$Entry#delete
-            method_20170 = WorldListWidget.Entry.class.getDeclaredMethod("method_20170", boolean.class);
-        } catch (NoSuchMethodException e) {
-            throw new RuntimeException(e);
-        }
-        method_20170.setAccessible(true);
-    }
-
     @Shadow
     private WorldListWidget levelList;
 
@@ -35,25 +19,11 @@ public abstract class SelectWorldScreenMixin extends Screen {
 
     @ModifyExpressionValue(method = "keyPressed", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;keyPressed(III)Z", ordinal = 0))
     public boolean keyPressed(boolean original, int keyCode, int scanCode, int modifiers) {
-        System.out.println(this.levelList.getSelected());
         if (original) return true;
         if (keyCode == GLFW.GLFW_KEY_DELETE) {
-            this.levelList.getSelectedAsOptional().ifPresent(this::tryDeleteWorld);
-            if (!this.levelList.children().isEmpty()) {
-                this.levelList.setSelected(this.levelList.children().get(0));
-                this.setFocused(this.levelList);
-            }
+            this.levelList.getSelectedAsOptional().ifPresent(WorldListWidget.WorldEntry::delete);
             return true;
         }
         return false;
-    }
-
-    @Unique
-    private void tryDeleteWorld(WorldListWidget.Entry entry) {
-        try {
-            method_20170.invoke(entry, true);
-        } catch (IllegalAccessException | InvocationTargetException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
---
+++ src/main/java/dev/tildejustin/delete_worlds/mixin/WorldListWidgetMixin.java
@@ -0,0 +1,22 @@
+package dev.tildejustin.delete_worlds.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.world.WorldListWidget;
+import net.minecraft.client.gui.widget.AlwaysSelectedEntryListWidget;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(WorldListWidget.class)
+public abstract class WorldListWidgetMixin extends AlwaysSelectedEntryListWidget<WorldListWidget.Entry> {
+    public WorldListWidgetMixin(MinecraftClient minecraftClient, int width, int height, int top, int bottom, int itemHeight) {
+        super(minecraftClient, width, height, top, bottom, itemHeight);
+    }
+
+    @Inject(method = "showSummaries", at = @At("TAIL"))
+    private void showSummaries(CallbackInfo ci) {
+        if (!this.children().isEmpty()) {
+            this.setFocused(this.children().get(0));
+        }
+    }
+}
